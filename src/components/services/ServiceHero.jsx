268c023d99c24f0ac21b9b9e85f1c3da8a57cc7d
Anam Aadel <rruholislam1998@gmail.com>
2023-11-10 11:06:51 +0600

    updated search box

--- src/components/services/ServiceHero.jsx
+++ src/components/services/ServiceHero.jsx
@@ -57,11 +57,11 @@ function ServiceHero({scroll}) {
               <input type="text"  name="search" id="search" className="p-3 block w-full border-transparent rounded-md focus:border-gray-500 focus:ring-blue-500 dark:bg-gray-800 dark:text-gray-400" placeholder="Search article" />
             </div>
             <div className="flex-[0_0_auto]">
-              <a className="p-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-700 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" href="#" >
+              <button type="submit" className="p-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-gray-700 text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800" >
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
                   <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
-              </a>
+              </button>
             </div>
           </div>
         </form>
